29f0a15016bc06cd4982f6e3c3108579b78d5bf9
Priya Tiwary <ptiwary304@gmail.com>
2024-06-16 22:42:55 +0530

    Initial commit

---
+++ netflix-stream/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+ 
+  return(
+    <h1 className="text-5xl font-bold text-red-500"> Hello ! </h1>
+  ) 
+    
+  
+}
+
+export default App
